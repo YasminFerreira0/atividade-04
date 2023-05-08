055a9ace223ebec3de1d13fec82f5546a9f27ef0
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-05-08 00:35:01 -0400

    up

--- questao-05.c
+++ questao-05.c
@@ -1,5 +1,54 @@
 #include <stdio.h>
 void main()
+/*Tentando descobrir se um dado era viciado, um dono de cassino o lançou 15 vezes.
+Para ajuda-lo, faça um programa que leia os 15 resultados dos lançamentos efetuados e
+calcule o número de ocorrências de cada face.*/
 {
+    int i, resultado, face1, face2, face3, face4, face5, face6;
+    face1 =0;
+    face2 = 0;
+    face3 = 0;
+    face4 = 0;
+    face5 = 0;
+    face6 = 0;
+
+    for(i=1; i<=15; i++){
+        printf("Informe o resultado do lancamento: ");
+        scanf("%d", &resultado);
+
+        switch (resultado)
+        {
+        case 1:
+            face1++;
+        break;
+        case 2:
+            face2++;
+        break;
+        case 3:
+            face3++;
+        break;
+        case 4:
+            face4++;
+        break;
+        case 5:
+            face5++;
+        break;
+        case 6:
+            face6++;
+        break;
+        
+        default:
+            printf("\nValor invalido!");
+        break;
+        }
+
+    }
+    printf("\nO numero de ocorrencia de cada face e: \n");
+    printf("Face 1: %d", face1);
+    printf("\nFace 2: %d", face2);
+    printf("\nFace 3: %d", face3);
+    printf("\nFace 4: %d", face4);
+    printf("\nFace 5: %d", face5);
+    printf("\nFace 6: %d", face6);
 
 }
